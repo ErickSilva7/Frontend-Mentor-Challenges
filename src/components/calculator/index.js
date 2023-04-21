5c97afa207f637521033c5f53a476cccfcbe2eda
ErickSilva7 <122052747+ErickSilva7@users.noreply.github.com>
2023-04-21 18:45:22 -0300

    Todas as teclas estão funcionando

--- src/components/calculator/index.js
+++ src/components/calculator/index.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./index.scss";
 
+let checkEqualOperation = false;
+
 const Calculator = () => {
     var keys = [
         { class: "number", key: "7" },
@@ -23,7 +25,10 @@ const Calculator = () => {
         { class: "equal", key: "=" },
     ];
 
-    const [num, setNum] = useState("");
+    let [num, setNum] = useState("0");
+    let [oldNum, setOldNum] = useState("");
+    let [operator, setOperator] = useState("");
+
     let [fontSize, setFontSize] = useState(3);
 
     const click = (btn) => {
@@ -31,6 +36,13 @@ const Calculator = () => {
         let btnInnerHTML = btn.target.innerHTML;
 
         if (btnClassName === "number") {
+            if (checkEqualOperation) {
+                checkEqualOperation = false;
+                setNum((num = "0"));
+                setOldNum((oldNum = ""));
+                setOperator((operator = ""));
+                setFontSize((fontSize = 3));
+            }
             if (num.length != 16) {
                 if (num.length > 7) {
                     setFontSize((fontSize -= 0.1875));
@@ -52,21 +64,87 @@ const Calculator = () => {
         }
 
         if (btnClassName === "del") {
-            setNum(num.slice(0, -1));
-            if (num.length > 8) {
-                setFontSize((fontSize += 0.1875));
+            if (checkEqualOperation) {
+                checkEqualOperation = false;
+                setNum((num = "0"));
+                setOldNum((oldNum = ""));
+                setOperator((operator = ""));
+                setFontSize((fontSize = 3));
+            }
+
+            if (num !== "0") {
+                setNum(num.slice(0, -1));
+                if (num.length === 1) {
+                    setNum((num = "0"));
+                }
+                if (num.length > 8) {
+                    setFontSize((fontSize += 0.1875));
+                }
             }
         }
 
         if (btnClassName === "operation") {
-            console.log(btnInnerHTML);
+            if (checkEqualOperation || oldNum === "") {
+                checkEqualOperation = false;
+                setOperator((operator = btnInnerHTML));
+                setOldNum((oldNum = num));
+                setNum((num = "0"));
+            } else {
+                var result;
+                if (operator === "+") {
+                    result = parseFloat(oldNum) + parseFloat(num);
+                } else if (operator === "-") {
+                    result = parseFloat(oldNum) - parseFloat(num);
+                } else if (operator === "x") {
+                    result = parseFloat(oldNum) * parseFloat(num);
+                } else {
+                    result = parseFloat(oldNum) / parseFloat(num);
+                }
+                setOldNum((oldNum = result));
+                setNum((num = "0"));
+                setOperator((operator = btnInnerHTML));
+                setFontSize((fontSize = 3));
+            }
+        }
+
+        if (btnClassName === "equal") {
+            var result;
+            if (operator === "+") {
+                setOldNum((oldNum = oldNum + " + " + num + " ="));
+                result = parseFloat(oldNum) + parseFloat(num);
+            } else if (operator === "-") {
+                setOldNum((oldNum = oldNum + " - " + num + " ="));
+                result = parseFloat(oldNum) - parseFloat(num);
+            } else if (operator === "x") {
+                setOldNum((oldNum = oldNum + " x " + num + " ="));
+                result = parseFloat(oldNum) * parseFloat(num);
+            } else {
+                setOldNum((oldNum = oldNum + " ÷ " + num + " ="));
+                result = parseFloat(oldNum) / parseFloat(num);
+            }
+            setNum((num = result));
+            setOperator((operator = ""));
+            checkEqualOperation = true;
+        }
+
+        if (btnClassName === "reset") {
+            setNum((num = "0"));
+            setOldNum((oldNum = ""));
+            setOperator((operator = ""));
+            setFontSize((fontSize = 3));
         }
     };
 
     return (
         <div>
-            <div className={"display"} style={{ fontSize: `${fontSize}rem` }}>
-                {num}
+            <div className={"display"}>
+                <div className="oldNumAndOperator">
+                    <div>{oldNum}</div>
+                    <div>{operator}</div>
+                </div>
+                <div className="num" style={{ fontSize: `${fontSize}rem` }}>
+                    {num}
+                </div>
             </div>
             <div className="keyboard">
                 {keys.map((props) => (
