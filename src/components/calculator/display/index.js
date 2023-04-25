439ddd206cd25c80d06e3c53179da6115b2ad49c
ErickSilva7 <122052747+ErickSilva7@users.noreply.github.com>
2023-04-24 22:08:07 -0300

    Update index.js

--- src/components/calculator/display/index.js
+++ src/components/calculator/display/index.js
@@ -2,7 +2,7 @@ import "./display.scss";
 
 const Display = ({ num, oldNum, operator }) => {
     return (
-        <div className="display" id="display">
+        <div className="display" >
             <div className="oldNumAndOperator">
                 <div>{oldNum}</div>
                 <div>{operator}</div>
