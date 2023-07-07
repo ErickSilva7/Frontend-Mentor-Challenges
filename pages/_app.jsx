f6e2f5d3a3a187386464f67a5131f1e8b6d55f83
ErickSilva7 <palmeiroerickmastery@gmail.com>
2023-07-07 15:22:03 -0300

    update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { ThemeProvider } from "../context/ThemeContext";
 
-// eslint-disable-next-line react/prop-types
 function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </ThemeProvider>
   );
